ca51302f22fbc58c27b122d1a967c2feafeee244
Vijaynalluri24 <157135773+Vijaynalluri24@users.noreply.github.com>
2024-01-24 17:02:24 +0530

    katzionleadlwc.js

--- katzionleadlwc/katzionleadlwc.js
+++ katzionleadlwc/katzionleadlwc.js
@@ -25,21 +25,6 @@ export default class Katzionleadlwc extends LightningElement {
     constructor()
     {
         super();
-        /*getallleads({ps:this.pagesize,cp:this.currentpage}).then(result=> {
-            this.no=result.length;
-            if(this.no==0)
-                {
-                    this.label='NO RECORDS FOUND/REACHED END OF DATA';
-                    this.data=result;
-                }
-                else{
-                    this.data=result;
-                    this.label='Number Of Records In The Page Are: '+this.no;
-                }
-    }).catch(error=> {
-            alert('Error in fetching the data while loading');
-            console.log(error);
-        })*/
         getleads({name:this.lname,filter:this.value,ps:this.pagesize,cp:this.currentpage}).then(result=> {
             this.no=result.length;
                 if(this.no==0)
@@ -94,12 +79,6 @@ export default class Katzionleadlwc extends LightningElement {
             this.currentpage--;
             getleads({name:this.lname,filter:this.value,ps:this.pagesize,cp:this.currentpage}).then(result=> {
                 this.no = result.length;
-                /*if(this.no==0)
-                {
-                    this.label='NO RECORDS FOUND/REACHED END OF DATA';
-                    this.data=result;
-                }
-                else{*/
                     this.label='Number Of Records In The Page Are: '+this.no;
                     this.data=result;
                     if(this.dis=true)
@@ -107,17 +86,6 @@ export default class Katzionleadlwc extends LightningElement {
                         
                         this.dis=false;
                     }
-                    
-                /*
-                    if(this.no<this.pagesize )
-                {
-                    this.dis=true;
-                }
-                else{
-                    this.dis=false;
-                }
-                }*/
-                
              }
                  
                  ).catch(error=> {
